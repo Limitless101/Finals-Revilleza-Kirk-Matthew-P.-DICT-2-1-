ad575e2429c64da1bbbb23900bb68fa84c9cc784
Limitless101 <118063340+Limitless101@users.noreply.github.com>
2023-02-22 01:05:39 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+class EmployeeTimeKeeping {
+    static void Main(string[] args) {
+        DateTime startTime = new DateTime(2023, 2, 21, 8, 0, 0); 
+        DateTime endTime = new DateTime(2023, 2, 21, 17, 0, 0); 
+        TimeSpan gracePeriod = new TimeSpan(0, 30, 0); 
+
+        
+        Console.Write("Enter employee time-in: ");
+        DateTime timeIn = DateTime.Parse(Console.ReadLine());
+        Console.Write("Enter employee time-out: ");
+        DateTime timeOut = DateTime.Parse(Console.ReadLine());
+
+        
+        Console.Write("Enter employee lunch break time-out: ");
+        DateTime lunchBreakTimeOut = DateTime.Parse(Console.ReadLine());
+        Console.Write("Enter employee lunch break time-in: ");
+        DateTime lunchBreakTimeIn = DateTime.Parse(Console.ReadLine());
+
+        
+        TimeSpan hoursWorked = timeOut - timeIn;
+
+        
+        DateTime regularStartTime = startTime.Add(gracePeriod);
+        DateTime regularEndTime = endTime.Subtract(gracePeriod);
+        TimeSpan regularHours = new TimeSpan();
+        if (timeIn < regularStartTime) {
+            regularHours = regularHours.Add(regularStartTime - timeIn);
+        }
+        if (timeOut > regularEndTime) {
+            regularHours = regularHours.Add(timeOut - regularEndTime);
+        }
+        regularHours = regularHours.Add(regularEndTime - regularStartTime);
+
+        
+        TimeSpan lunchBreakHours = new TimeSpan();
+        if (timeIn < lunchBreakTimeOut && timeOut > lunchBreakTimeIn) {
+            lunchBreakHours = lunchBreakTimeIn - lunchBreakTimeOut;
+        }
+
+        
+        TimeSpan lateTime = new TimeSpan();
+        if (timeIn > regularStartTime) {
+            lateTime = timeIn - regularStartTime;
+        }
+
+        
+        TimeSpan undertime = new TimeSpan();
+        if (timeOut < regularEndTime) {
+            undertime = regularEndTime - timeOut;
+        }
+
+        
+        TimeSpan overtime = new TimeSpan();
+        if (timeOut > endTime) {
+            overtime = timeOut - endTime;
+        }
+
+        
+        Console.WriteLine("Hours worked: " + hoursWorked.TotalHours);
+        Console.WriteLine("Regular hours worked: " + regularHours.TotalHours);
+        Console.WriteLine("Lunch break hours: " + lunchBreakHours.TotalHours);
+        Console.WriteLine("Late time: " + lateTime.TotalMinutes + " minutes");
+        Console.WriteLine("Undertime: " + undertime.TotalMinutes + " minutes");
+        Console.WriteLine("Overtime: " + overtime.TotalHours + " hours");
+
+        
+        Console.ReadLine();
+    }
+}
